7a5470daea7c77b1691bab12e8563dfb0f3d56fe
Haoming <harrylai@live.com>
2018-03-05 19:25:11 -0500

    ML&gesture control: Logistic reg finished but not done

--- HoloscreenII/holoscreen_android/Assets/Scripts/GestureControl.cs
+++ HoloscreenII/holoscreen_android/Assets/Scripts/GestureControl.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//new
+using System;
+using System.IO;
+using OpenCVForUnity;
 
 public class GestureControl : MonoBehaviour {
 	//Left hand finger declare
 	private GameObject palm, indexfinger, indexfinger_bone1, indexfinger_bone2;
 	public bool Pose = false;
 
+	//poseDetectorMLtrainning
+	LogisticRegression logis_reg_model;
+	int gesture_num = 3;
+	int mat_n = 15;
 
 
 	// Use this for initialization
@@ -15,6 +23,9 @@ public class GestureControl : MonoBehaviour {
 		indexfinger_bone1 = this.transform.GetChild (1).GetChild (1).gameObject;
 		indexfinger_bone2 = this.transform.GetChild (1).GetChild (2).gameObject;
 		palm = this.transform.GetChild (5).gameObject;
+
+		//new
+		poseDetectorMLtrainning();
 	}
 	
 	// Update is called once per frame
@@ -24,7 +35,7 @@ public class GestureControl : MonoBehaviour {
 		else
 			Pose = false;
 
-		handDataGenerator ();
+		//handDataGenerator ();
 	}
 
 	private bool checkPosePointing(){
@@ -47,6 +58,59 @@ public class GestureControl : MonoBehaviour {
 
 		return false;
 	}
+
+	/* 	poseDetectorMLtrainning
+	*	Input: GameObject obj
+	*	Output: None
+	*	Summary: 1. Process text file to OpenCV mat 2.train the model and store in disk
+	*/
+	private void poseDetectorMLtrainning(){
+		//Data process
+		string data_fname_basic = "Assets/Data/handData_G";
+		Mat all_data = new Mat();
+		Mat all_label = new Mat ();
+		for (int i = 0; i < 2; i++) {
+			string data_fname = data_fname_basic + i.ToString () + ".txt";
+			Debug.Log (data_fname);
+			int mat_m = File.ReadAllLines (data_fname).Length;
+			Mat cur_data = Mat.zeros(mat_m,mat_n,CvType.CV_32F);
+			Mat cur_label = Mat.ones(mat_m, 1, CvType.CV_32F);
+			Core.multiply(cur_label,new Scalar((double)(i+2)),cur_label);
+
+			StreamReader reader = new StreamReader (data_fname);
+			int row = 0;
+			while(reader.Peek() >= 0) {
+				string cur_line = reader.ReadLine();
+				float[] cur_line_data = Array.ConvertAll (cur_line.Split (','), float.Parse);
+				cur_data.put(row,0,cur_line_data);
+				row++;
+			}
+			all_data.push_back (cur_data);
+			all_label.push_back (cur_label);
+			reader.Close ();
+		}
+
+		logis_reg_model = OpenCVForUnity.LogisticRegression.create ();
+		TrainData data_lable = OpenCVForUnity.TrainData.create (all_data, OpenCVForUnity.Ml.ROW_SAMPLE,all_label);
+		Debug.Log(logis_reg_model.train(data_lable));
+		//Debug.Log(logis_reg_model.calcError(data_lable,true,all_label));
+		//temp - test
+		Mat result = Mat.zeros(1,1,CvType.CV_32S);
+		//it should output 0
+		int idx = 100;
+		logis_reg_model.predict (all_data.row (idx), result, 0);
+		Debug.Log ("Data is: " + all_data.get(idx,1)[0]);
+		Debug.Log ("Label should be: " + all_label.get(idx,0)[0]);
+		Debug.Log ("Predicted label is: " + result.get (0, 0) [0]);
+		//test - test
+		return;
+	}
+
+	private int poseDetectorMLtesting(){
+		
+		return 0;
+	}
+	//new
 
 	private void handDataGenerator(){
 		Vector3[] vec_bone2 = new Vector3[5];
